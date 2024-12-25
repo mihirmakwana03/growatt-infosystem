584b937b4a5e55a6a05e1cb16a88ea76ca1fe860
Sahil Mondal <sahilmondal9879@gmail.com>
2024-12-25 22:45:56 +0530

    Sidebar Created

---
+++ src/components/sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from './img/logo.png';
+import './sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div
+      className="d-flex flex-column flex-shrink-0 p-3 bg-light"
+      style={{ width: '250px', height: '100vh' }}
+    >
+      <NavLink
+        to="/adminhome"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+      >
+        <a href="index.html">
+          <img src={Logo} className="img-fluid" alt="logo" />
+        </a>
+      </NavLink>
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <NavLink to="/adminhome" className="nav-link link-dark" activeClassName="active" exact>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/adminportfolio" className="nav-link link-dark" activeClassName="active">
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/adminservices" className="nav-link link-dark" activeClassName="active">
+            Service
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admincareer" className="nav-link link-dark" activeClassName="active">
+            Career
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Admin from './Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+
+    {/* Comment out the Admin component  */}
+    {/* <Admin />*/}
   </React.StrictMode>
 );
