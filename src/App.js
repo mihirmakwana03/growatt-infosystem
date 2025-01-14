211dc13cb67efee108338d870f59e8c7e057981b
Mihir Makwana <mihirpmakwana786@gmail.com>
2025-01-14 22:54:16 +0530

    Demo Complete

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import React from 'react'
 import "./style.css"
 import { OrbitControls } from '@react-three/drei'
 import Scene from './components/scene'
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
 
 const App = () => {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight />
-      <Scene />
-    </Canvas>
+    <>
+      <Canvas flat camera={{ fov: 60 }}>
+        {/* <OrbitControls /> */}
+        <ambientLight />
+        <Scene />
+        <EffectComposer>
+          <Bloom
+            mipmapBlur
+            intensity={7.0}
+            luminanceThreshold={0}
+            luminanceSmoothing={0}
+          />
+          {/* <ToneMapping adaptive /> */}
+        </EffectComposer>
+      </Canvas>
+      <div className='div w-full bg-black py-32'>
+        <h1 className='text-white text-center'>Hello World!</h1>
+      </div>
+    </>
   )
 }
 
--- src/components/scene.js
+++ src/components/scene.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useTexture } from '@react-three/drei'
 import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
 
 const Scene = () => {
     let tex = useTexture("./image.jpg")
+    let cyl = useRef(null)
+    useFrame((state, delta) => {
+        cyl.current.rotation.y += delta;
+    })
     return (
-            <mesh>
-                <cylinderGeometry args={[1, 1, 1, 30, 30, true]} />
-                <meshStandardMaterial map={tex} side={THREE.DoubleSide} />
+        <group rotation={[0, 1.5, 0.5]}>
+            <mesh ref={cyl}>
+                <cylinderGeometry args={[2, 2, 2, 60, 60, true]} />
+                <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
             </mesh>
+        </group>
     )
 }
 
